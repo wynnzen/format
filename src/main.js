8aea6368b08af3ee0db3c05753e491cfb768ce3a
wynnzen <5346458+wynnzen@users.noreply.github.com>
2021-08-11 11:15:17 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(VueCompositionAPI)
+Vue.use(Antd);
 
 Vue.config.productionTip = false;
 
