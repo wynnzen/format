d6bb771bbe68adc5a37401165429c285c4ae78cc
wynnzen <5346458+wynnzen@users.noreply.github.com>
2021-08-11 16:30:42 +0800

    refactor header.vue to jsx

--- src/views/formatter/components/header.vue
+++ src/views/formatter/components/header.vue
@@ -1,89 +1,64 @@
-<template>
-  <a-row class="header" type="flex" justify="space-between">
-    <a-col>
-      <a-space>
-        <a-radio-group default-value="json5" size="small" v-model="fmtype">
-          <a-radio-button value="json5"> json5 </a-radio-button>
-          <a-radio-button value="json"> json </a-radio-button>
-        </a-radio-group>
-        <span v-if="!actived" class="file-name" @click="inputActive">{{ fileName }}{{ ext }}</span>
-        <a-input size="small" v-else v-model="fileName" @blur="inputDeActive" @pressEnter="inputDeActive"></a-input>
-      </a-space>
-    </a-col>
-    <a-col>
-      <a-space>
-        <a-button size="small" @click="fullScreen">全屏</a-button>
-        <a-upload :file-list="fileList" :remove="handleRemove" :showUploadList="false" :before-upload="beforeUpload">
-          <a-button size="small" @click="importCode">导入</a-button>
-        </a-upload>
-        <a-button size="small" @click="exportCode">导出</a-button>
-        <a-button size="small" @click="copy">复制</a-button>
-        <a-button size="small" @click="empty">清空</a-button>
-      </a-space>
-    </a-col>
-  </a-row>
-</template>
 <script>
-import { computed, ref, watch } from '@vue/composition-api'
+import { computed, defineComponent, ref, watch } from "@vue/composition-api";
 
-export default {
-  emits: ['full-screen', 'empty', 'copy', 'import-code', 'export-code'],
-  setup (props, context) {
-    const fmtype = ref('json5')
-    const actived = ref(false)
-    const fileName = ref('config')
-    const fileList = ref([])
+export default defineComponent({
+  emits: ["full-screen", "empty", "copy", "import-code", "export-code"],
+  setup(props, context) {
+    const fmtype = ref("json5");
+    const actived = ref(false);
+    const fileName = ref("config");
+    const fileList = ref([]);
 
     const ext = computed(() => {
-      return fmtype.value === 'json' ? '.json' : '.js'
-    })
+      return fmtype.value === "json" ? ".json" : ".js";
+    });
 
     watch(fmtype, (val, oldVal) => {
       if (val !== oldVal) {
-        context.emit('update-type', val)
+        context.emit("update-type", val);
       }
-    })
+    });
 
     const data = {
       ext,
       fmtype,
       actived,
       fileName,
-      fileList
-    }
+      fileList,
+    };
 
     const fullScreen = () => {
-      context.emit('full-screen')
-    }
+      context.emit("full-screen");
+    };
 
     const inputActive = () => {
-      actived.value = true
-    }
+      actived.value = true;
+    };
     const inputDeActive = () => {
-      actived.value = false
-    }
+      actived.value = false;
+    };
 
     const empty = () => {
-      context.emit('empty')
-    }
+      context.emit("empty");
+    };
     const copy = () => {
-      context.emit('copy')
-    }
-    const handleRemove = () => {}
+      context.emit("copy");
+    };
+    const handleRemove = () => {};
     const beforeUpload = (file) => {
-      fileList.value = [...fileList.value, file]
-      const reader = new FileReader()
+      fileList.value = [...fileList.value, file];
+      const reader = new FileReader();
       reader.onload = (evt) => {
-        const content = evt.target.result
-        context.emit('import-code', content)
-      }
-      reader.readAsText(file)
-      return false
-    }
-    const importCode = () => {}
+        const content = evt.target.result;
+        context.emit("import-code", content);
+      };
+      reader.readAsText(file);
+      return false;
+    };
+    const importCode = () => {};
     const exportCode = () => {
-      context.emit('export-code')
-    }
+      context.emit("export-code");
+    };
 
     const methods = {
       fullScreen,
@@ -94,18 +69,123 @@ export default {
       handleRemove,
       beforeUpload,
       importCode,
-      exportCode
-    }
+      exportCode,
+    };
 
     return {
       ...data,
-      ...methods
-    }
-  }
-}
+      ...methods,
+    };
+  },
+
+  render() {
+    const {
+      fullScreen,
+      inputActive,
+      inputDeActive,
+      empty,
+      copy,
+      handleRemove,
+      beforeUpload,
+      importCode,
+      exportCode,
+    } = this;
+
+    const { ext, fmtype, actived, fileName, fileList } = this;
+    return (
+      <a-row class="header" type="flex" justify="space-between">
+        <a-col>
+          <a-space>
+            <a-radio-group default-value="json5" size="small" vModel={fmtype}>
+              <a-radio-button value="json5"> json5 </a-radio-button>
+              <a-radio-button value="json"> json </a-radio-button>
+            </a-radio-group>
+            {!actived ? (
+              <span class="file-name" onClick={inputActive}>
+                {fileName}
+                {ext}
+              </span>
+            ) : (
+              <a-input
+                size="small"
+                vModel={fileName}
+                onBlur={inputDeActive}
+                onPressEnter={inputDeActive}
+              ></a-input>
+            )}
+          </a-space>
+        </a-col>
+        <a-col>
+          <a-space>
+            <a-button size="small" onClick={fullScreen}>
+              全屏
+            </a-button>
+            <a-upload
+              file-list={fileList}
+              remove={handleRemove}
+              showUploadList={false}
+              before-upload={beforeUpload}
+            >
+              <a-button size="small" onClick={importCode}>
+                导入
+              </a-button>
+            </a-upload>
+            <a-button size="small" onClick={exportCode}>
+              导出
+            </a-button>
+            <a-button size="small" onClick={copy}>
+              复制
+            </a-button>
+            <a-button size="small" onClick={empty}>
+              清空
+            </a-button>
+          </a-space>
+        </a-col>
+      </a-row>
+    );
+  },
+});
 </script>
 <style lang="less" scoped>
 .header {
   margin-bottom: 8px;
 }
 </style>
+<!-- <template>
+  <a-row class="header" type="flex" justify="space-between">
+    <a-col>
+      <a-space>
+        <a-radio-group default-value="json5" size="small" v-model="fmtype">
+          <a-radio-button value="json5"> json5 </a-radio-button>
+          <a-radio-button value="json"> json </a-radio-button>
+        </a-radio-group>
+        <span v-if="!actived" class="file-name" onClick="inputActive"
+          >{{ fileName }}{{ ext }}</span
+        >
+        <a-input
+          size="small"
+          v-else
+          v-model="fileName"
+          @blur="inputDeActive"
+          @pressEnter="inputDeActive"
+        ></a-input>
+      </a-space>
+    </a-col>
+    <a-col>
+      <a-space>
+        <a-button size="small" onClick="fullScreen">全屏</a-button>
+        <a-upload
+          :file-list="fileList"
+          :remove="handleRemove"
+          :showUploadList="false"
+          :before-upload="beforeUpload"
+        >
+          <a-button size="small" onClick="importCode">导入</a-button>
+        </a-upload>
+        <a-button size="small" onClick="exportCode">导出</a-button>
+        <a-button size="small" onClick="copy">复制</a-button>
+        <a-button size="small" onClick="empty">清空</a-button>
+      </a-space>
+    </a-col>
+  </a-row>
+</template> -->
